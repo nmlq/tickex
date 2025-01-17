5d69c3ae424e35d82769708a16bdbb978da8c221
nmq <multiloquant@gmail.com>
2025-01-17 15:11:17 -0800

    basic ticker object with json export

--- tickex/cli.py
+++ tickex/cli.py
@@ -1,10 +1,32 @@
-import yfinance as yf
-import datetime as dt
+import datetime
+from tickex import data
+import json
+
 
 def main():
-    print("tickex main")
-    start_date = dt.datetime.today()- dt.timedelta(1000) 
-    end_date = dt.datetime.today()
-    stock ="USDJPY=X"
-    data = yf.download(stock, start_date, end_date)
-    print(data)
+    today = datetime.datetime.today()
+    # Get everything you can from 5 years ago daily interval
+    years_start_1d_interval = datetime.timedelta(days=1) #(days=365*5) 
+    # Get everything you can from 60 days ago 15m interval
+    days_start_15m_interval = datetime.timedelta(days=1)#(days=60)
+    
+    day_tickers = data.get_tickers(
+        start_date=today - years_start_1d_interval,
+        end_date=today,
+        interval='1d'
+    )
+    for t in day_tickers:
+        filename = f"{t.name}_{t.interval}_{t.timestamp}.json"
+        with open(filename, 'w') as jf:
+            json.dump(t.to_dict(), jf, indent=2)
+
+    intraday_tickers = data.get_tickers(
+        start_date=today - days_start_15m_interval,
+        end_date=today,
+        interval='15m'
+    )
+
+    for t in intraday_tickers:
+        filename = f"{t.name}_{t.interval}_{t.timestamp}.json"
+        with open(filename, 'w') as jf:
+            json.dump(t.to_dict(), jf, indent=2)
---
+++ tickex/types.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, asdict
+
+
+@dataclass
+class Ticker:
+    name: str
+    timestamp: str
+    close: float
+    high: float
+    low: float
+    open: float
+    volume: float
+    interval: str
+
+    def to_dict(self):
+        return asdict(self)
+    
+    @classmethod
+    def from_dict(cls, dictionary):
+        return cls(**dictionary)
+
+
